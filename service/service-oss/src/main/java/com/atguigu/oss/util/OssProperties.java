085d72d8cbfcaa050f993fcceb21d6a58ecd4b38
root <2802612334@qq.com>
2021-02-20 17:17:50 +0800

    头像上传控制器修改

--- service/service-oss/src/main/java/com/atguigu/oss/util/OssProperties.java
+++ service/service-oss/src/main/java/com/atguigu/oss/util/OssProperties.java
@@ -2,7 +2,6 @@ package com.atguigu.oss.util;
 
 import lombok.Data;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
